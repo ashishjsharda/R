c80f259a8a535b8d219dc0984d67cb6524a129b8
Ashish S <ashishjsharda@gmail.com>
2021-01-25 07:11:02 -0500

    Create popup.R

---
+++ popup.R
@@ -0,0 +1,11 @@
+library(htmltools)
+
+df <- read.csv(textConnection(
+"Name,Lat,Long
+Samurai Noodle,47.597131,-122.327298
+Kukai Ramen,47.6154,-122.327157
+Tsukushinbo,47.59987,-122.326726"
+))
+
+leaflet(df) %>% addTiles() %>%
+  addMarkers(~Long, ~Lat, popup = ~htmlEscape(Name))
