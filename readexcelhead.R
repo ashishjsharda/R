8080b3c96c6159f35ef0f4a279f8973362bd78ff
Ashish S <ashishjsharda@gmail.com>
2021-02-06 19:03:24 -0500

    Create readexcelhead.R

---
+++ readexcelhead.R
@@ -0,0 +1,5 @@
+download.file("https://jaredlander.com/data/ExcelExample.xlsx",destfile = "ExcelExample.xlsx",mode="wb")
+library(readxl)
+tomatoXsl<-read_excel("ExcelExample.xlsx",sheet = 1)
+tomatoXsl
+head(tomatoXsl)
