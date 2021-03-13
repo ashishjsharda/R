5f92c99a7e1abf4ca6a4dacb79eaecd917c064ac
Ashish S <ashishjsharda@gmail.com>
2021-03-13 17:12:47 -0500

    Create latex.R

---
+++ latex.R
@@ -0,0 +1,14 @@
+\documentclass{article}
+
+\usePackage{graphicx}
+\DeclaredGraphicsExtension{.png,.jpg}
+\newcommand {\dataframe}{\texttt{data.frame}}
+\author {Ashish Sharda}
+\title{Simple Article}
+\begin{document}
+\SweaveOpts{concordance=TRUE}
+
+
+
+
+\end{document}
