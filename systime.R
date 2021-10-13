5eff6ea1d64103f1d01aa77a4a583669cf431a2c
Ashish S <ashishjsharda@gmail.com>
2021-10-13 18:26:45 -0400

    Create systime.R

---
+++ systime.R
@@ -0,0 +1,11 @@
+abs_loop <- function(vec){
+  for (i in 1:length(vec)) {
+    if (vec[i] < 0) {
+      vec[i] <- -vec[i]
+    }
+  }
+  vec
+}
+long <- rep(c(-1, 1), 5000000) 
+long
+system.time(abs_loop(long))
