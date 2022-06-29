c285baaa935c9f5edf1735c5fde522d72d60c23b
Ashish S <ashishjsharda@gmail.com>
2022-06-29 12:25:59 -0500

    Create baseplot.R

---
+++ baseplot.R
@@ -0,0 +1,6 @@
+plot(iris$Petal.Length, iris$Petal.Width,
+     main = "Petal Width vs. Length",
+     sub = "iris dataset",
+     xlab = "Petal Length",
+     ylab = "Petal Width",
+     col = "blue")
