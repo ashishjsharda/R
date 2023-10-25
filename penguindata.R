63877bf046d0c3b98a37848a38f8cf84852b3202
Ashish Sharda <ashishjsharda@gmail.com>
2023-10-25 06:44:46 -0400

    Create penguindata.R
    https://rpubs.com/ashishj_sharda/pengundata

---
+++ penguindata.R
@@ -0,0 +1,10 @@
+library(palmerpenguins)
+library(ggthemes)
+library(ggplot2)
+ggplot(data = penguins)
+ggplot(
+  data = penguins,
+  mapping = aes(x = flipper_length_mm, y = body_mass_g)
+) +
+  geom_point(mapping = aes(color = species)) +
+  geom_smooth(method = "lm")
