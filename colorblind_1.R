d3f6b90d8819ce46941e49894cad1251f7ef3e5a
Ashish Sharda <ashishjsharda@gmail.com>
2023-10-31 07:27:05 -0400

    Create colorblind_1.R
    https://rpubs.com/ashishj_sharda/colorblind_safe

---
+++ colorblind_1.R
@@ -0,0 +1,16 @@
+library(palmerpenguins)
+library(ggthemes)
+library(ggplot2)
+ggplot(
+  data = penguins,
+  mapping = aes(x = flipper_length_mm, y = body_mass_g)
+) +
+  geom_point(aes(color = species, shape = species)) +
+  geom_smooth(method = "lm") +
+  labs(
+    title = "Body mass and flipper length",
+    subtitle = "Dimensions for Adelie, Chinstrap, and Gentoo Penguins",
+    x = "Flipper length (mm)", y = "Body mass (g)",
+    color = "Species", shape = "Species"
+  ) +
+  scale_color_colorblind()
