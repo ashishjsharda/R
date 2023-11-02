cf8d6df19eab307b9761d820d0fdaf7d32509ac8
Ashish Sharda <ashishjsharda@gmail.com>
2023-11-02 07:34:27 -0400

    Create geom_histogram.R
    https://rpubs.com/ashishj_sharda/geom_histogram

---
+++ geom_histogram.R
@@ -0,0 +1,7 @@
+library(palmerpenguins)
+library(ggthemes)
+library(ggplot2)
+ggplot(penguins, aes(x = body_mass_g)) +
+  geom_histogram(binwidth = 20)
+ggplot(penguins, aes(x = body_mass_g)) +
+  geom_histogram(binwidth = 2000)
