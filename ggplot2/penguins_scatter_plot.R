54bf4c0773149d2f56f3fe96b1d5f06cb36f2249
Ashish Sharda <ashishjsharda@gmail.com>
2023-12-22 07:01:53 -0500

    Create penguins_scatter_plot.R
    https://rpubs.com/ashishj_sharda/pscatter_plot

---
+++ ggplot2/penguins_scatter_plot.R
@@ -0,0 +1,12 @@
+library(palmerpenguins)
+library(ggthemes)
+library(ggplot2)
+ggplot(
+  data = penguins,
+  mapping = aes(
+    x = bill_length_mm, y = bill_depth_mm, 
+    color = species, shape = species
+  )
+) +
+  geom_point() +
+  labs(color = "Species")
