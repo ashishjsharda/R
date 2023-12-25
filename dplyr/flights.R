4937d0efde2be301e6fcf062ac2ba794cd0979cc
Ashish Sharda <ashishjsharda@gmail.com>
2023-12-25 10:19:19 -0500

    Create flights.R

---
+++ dplyr/flights.R
@@ -0,0 +1,10 @@
+library(nycflights13)
+library(tidyverse)
+flights
+glimpse(flights)
+flights |>
+  filter(dest == "IAH") |> 
+  group_by(year, month, day) |> 
+  summarize(
+    arr_delay = mean(arr_delay, na.rm = TRUE)
+  )
