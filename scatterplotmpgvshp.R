cda43013001bacef3cd0e16cd9958d1c1815e183
Ashish Sharda <ashishjsharda@gmail.com>
2024-04-20 13:09:02 -0400

    Create scatterplotmpgvshp.R
    https://rpubs.com/ashishj_sharda/scatterplotmpgvshp

---
+++ scatterplotmpgvshp.R
@@ -0,0 +1,7 @@
+data("mtcars")
+plot(mtcars$hp,mtcars$mpg,
+     main = "Scatterplot of mpg vs horsepower",
+     xlab = "Horsepower",
+     ylab = "Miles per gallon",
+      col ="blue",
+     pch=16)
